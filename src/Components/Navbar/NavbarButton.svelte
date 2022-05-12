145588455cf3343f9103885f8771c966d6135698
EliasVal <46247153+EliasVal@users.noreply.github.com>
2022-05-12 08:44:43 +0300

    Improved navbar - Added transition animation - Changed highlighter background-color

--- src/Components/Navbar/NavbarButton.svelte
+++ src/Components/Navbar/NavbarButton.svelte
@@ -8,16 +8,14 @@
   let highlight = false;
 
   $: {
-    // if ($LocationHash == id) {
-    //   highlight = true;
-    // }
-
     highlight = $LocationHash == id;
   }
 </script>
 
 <div class:highlight on:click={() => window.location.assign(`#${id}`)}>
-  <h1>{char}</h1>
+  <div>
+    <h1>{char}</h1>
+  </div>
   <span>
     {tooltip}
   </span>
@@ -27,18 +25,6 @@
   @import "src/scss/_mixins.scss";
 
   div {
-    @include unselectable;
-    @include flex-center;
-
-    cursor: pointer;
-
-    aspect-ratio: 1 / 1;
-    width: 3rem;
-    height: auto;
-    border-radius: 50%;
-
-    background-color: white;
-
     position: relative;
 
     &:hover span {
@@ -46,41 +32,64 @@
       opacity: 1;
       padding-left: 60%;
     }
-    span {
-      display: flex;
-      align-items: center;
-
-      background-color: black;
-      color: white;
-      font-size: 1.5rem;
-
-      border-radius: 20px 15px 15px 20px;
-
-      position: absolute;
-      top: 0.3rem;
-      bottom: 0.3rem;
-      left: 0;
-
-      opacity: 0;
-      padding-right: 20%;
-      padding-left: 0%;
-
-      z-index: -1;
-
-      transition: 0.3s ease-out;
-
-      pointer-events: none;
-    }
-    h1 {
-      color: black;
-
-      line-height: 1;
-      font-size: 1.6rem;
-    }
 
     &.highlight {
-      width: 4rem;
-      background-color: hsl(246, 88%, 27%);
+      span {
+        left: 60%;
+      }
+      div {
+        transform: scale(1.3);
+        background-color: hsl(212, 80%, 48%);
+      }
     }
+
+    > div {
+      @include unselectable;
+      @include flex-center;
+
+      cursor: pointer;
+
+      aspect-ratio: 1 / 1;
+      width: 3rem;
+      height: auto;
+      border-radius: 50%;
+
+      background-color: white;
+
+      transition: transform 0.25s;
+
+      h1 {
+        color: black;
+
+        line-height: 1;
+        font-size: 1.6rem;
+      }
+    }
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+
+    background-color: black;
+    color: white;
+    font-size: 1.5rem;
+
+    border-radius: 0 5px 5px 0;
+
+    position: absolute;
+    top: 0.3rem;
+    bottom: 0.3rem;
+    left: 0;
+
+    opacity: 0;
+    padding-right: 15%;
+    padding-left: 0%;
+
+    z-index: -1;
+
+    transition: 0.3s ease-out;
+
+    pointer-events: none;
   }
 </style>
