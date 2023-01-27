46cad10769186a6d080ed28521c0beeb4a9692ad
EliasVal <46247153+EliasVal@users.noreply.github.com>
2023-01-27 13:54:16 +0200

    about

--- src/Components/AboutMe.svelte
+++ src/Components/AboutMe.svelte
@@ -1,10 +1,68 @@
-<p>
-	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos et, repellat commodi nihil labore
-	facilis quis dolores provident nam debitis voluptatibus doloremque aperiam saepe rem perferendis
-	veritatis quo. Voluptate, error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-	adipisci temporibus aspernatur quia perspiciatis vel ex, exercitationem tenetur quasi suscipit
-	nulla at! Iste doloremque unde a repudiandae sequi, ab assumenda? Lorem ipsum dolor sit amet
-	consectetur adipisicing elit. Laudantium aspernatur officia libero tempore neque qui laboriosam
-	odio ratione voluptates, distinctio, voluptatibus non illum optio, quod quisquam earum? Maxime,
-	Pbeatae ipsa.
-</p>
+<script lang="ts">
+	import { fly, fade } from 'svelte/transition';
+
+	import { cubicOut } from 'svelte/easing';
+
+	const age: number =
+		// @ts-ignore
+		new Date(new Date() - new Date(2005, 11, 25)).getFullYear() - 1970;
+</script>
+
+<div id="about">
+	<div id="header">
+		<div in:fly={{ x: -60, duration: 850, easing: cubicOut, delay: 100 }} id="title">
+			A little bit
+			<br />
+			<div in:fly={{ x: -50, duration: 750, delay: 200, easing: cubicOut }} id="subtitle">
+				About Me
+			</div>
+		</div>
+	</div>
+	<div id="content">
+		<p>
+			Hello! I'm Elias! I'm a {age} year old developer, and I make anything from Websites, to games to
+			Discord bots. I've been programming since 11, starting off with web development with HTML and CSS.
+			I've learned C# at 12 and got into game development using the Unity Game Engine. Nowadays, I use:
+			Languages: C#, JavaScript & TypeScript, HTML, CSS & SCSS Frameworks/Libraries: discord.js (v12,
+			v13), Svelte & SvelteKit, React, Firebase, ASP.NET, Electron
+		</p>
+	</div>
+</div>
+
+<style lang="scss">
+	#about {
+		height: 100%;
+
+		#header {
+			margin: 4em 5em;
+			background-color: transparent;
+			#title {
+				font-size: 4em;
+				font-weight: bold;
+
+				#subtitle {
+					font-size: 1.75em;
+					background: linear-gradient(
+						to right,
+						rgb(235, 116, 19) 40%,
+						rgb(212, 124, 51),
+						rgb(226, 146, 81)
+					);
+					background-clip: text;
+					-webkit-text-fill-color: transparent;
+
+					margin-left: 0.75rem;
+				}
+			}
+			display: inline-block;
+		}
+
+		#content {
+			> p {
+				max-width: 60ch;
+				margin: 0 auto;
+				font-size: 1.3em;
+			}
+		}
+	}
+</style>
--- src/Components/MainHeader.svelte
+++ src/Components/MainHeader.svelte
@@ -1,49 +1,70 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
+	import { fly, fade } from 'svelte/transition';
 
 	import { cubicOut } from 'svelte/easing';
 </script>
 
 <main>
-	<div id="main-content">
-		<div in:fly={{ x: -60, duration: 850, easing: cubicOut, delay: 100 }} id="title">
-			Web Development
+	<!-- <div id="main-content"> -->
+	<!-- <div in:fly={{ x: -60, duration: 850, easing: cubicOut, delay: 100 }} id="title">
+			Take your website to the
 			<br />
 			<div in:fly={{ x: -50, duration: 750, delay: 200, easing: cubicOut }} id="reimagined">
-				Reimagined
+				Next Level
 			</div>
-		</div>
+		</div> -->
+	<div id="main-content">
+		<h1 id="title" in:fly={{ y: -150, duration: 1850, easing: cubicOut, delay: 100 }}>
+			- Welcome -
+		</h1>
+		<h2 in:fade={{ duration: 1750, delay: 1950, easing: cubicOut }}>
+			To my <span id="portfolio">Portfolio</span>.
+		</h2>
 	</div>
+	<!-- </div> -->
 </main>
 
 <style lang="scss">
 	main {
-		height: 100%;
+		height: 100vh;
+		width: 100%;
+
+		display: flex;
+
+		align-items: center;
+		justify-content: center;
 
 		#main-content {
-			margin: 4em 5em;
-			background-color: transparent;
+			// 	width: 100%;
+			// 	height: fit-content;
+			// 	// margin: 4em 5em;
+			// 	margin: 0 auto;
+			// 	background-color: transparent;
 			#title {
-				font-size: 4em;
+				font-size: 6em;
 				font-weight: bold;
 
-				#reimagined {
-					font-size: 1.75em;
+				// text-align: center;
+			}
+			h2 {
+				text-align: center;
+				#portfolio {
+					// font-size: 1.75em;
 					background: linear-gradient(
 						to right,
 						rgb(235, 116, 19) 40%,
-						rgb(212, 124, 51),
-						rgb(226, 146, 81)
+						rgb(231, 156, 94),
+						rgb(240, 151, 18)
 					);
 					background-clip: text;
 					-webkit-text-fill-color: transparent;
 
-					margin-left: 0.75rem;
+					// margin-left: 0.75rem;
 				}
 			}
-			z-index: 2;
-			position: relative;
-			display: inline-block;
+			// z-index: 2;
+			// position: relative;
+			// display: inline-block;
 		}
 	}
 </style>
