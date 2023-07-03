6cda52e16cf7afe443a624f7db42bda65b4ab4c2
EliasVal <46247153+EliasVal@users.noreply.github.com>
2023-07-03 03:16:12 +0300

    Added footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,17 +10,8 @@
   import AboutMe from '../Components/Sections/AboutMe.svelte';
   import NextSection from '../Components/NextSection.svelte';
 
-  // onMount(() => {
-  // 	fontResize();
-  // });
-
-  // function fontResize() {
-  // 	let perc = window.outerWidth / (window.outerWidth <= 680 ? 9 : 15);
-  // 	document.getElementsByTagName('body')[0].style.fontSize = `${perc}%`;
-  // }
+  import Footer from '../Components/Footer.svelte';
 </script>
-
-<!-- <svelte:window on:resize={fontResize} on:focus={fontResize} on:blur={fontResize} /> -->
 
 <IntersectionObserver>
   <MainHeader />
@@ -37,5 +28,7 @@
 <IntersectionObserver>
   <Projects />
 </IntersectionObserver>
+
+<Footer />
 
 <NextSection />
